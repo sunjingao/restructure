d10f997b74ae5d1918bc5aff3d8d164b772b7dac
jingao.sun <jingao.sun@faw-vw.com>
2025-01-14 17:41:22 +0800

    增加文档的loading展示

--- dist/assets/index-BGiy_hrt.js
+++
@@ -1,1 +0,0 @@
-import{g as e}from"./index--BEtZTdE.js";import{o as s,c as t,u as r}from"./index-BmZqEalr.js";const c=["src"],f={__name:"index",setup(o){return(a,n)=>(s(),t("iframe",{class:"w-[100%] h-[100%]",src:r(e)("/pdf/step4.pdf")},null,8,c))}};export{f as default};
